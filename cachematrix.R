9a515f97724f4f3569a8bd33751ebc36e74ee7b6
Abdulkadir Yaman <abdulkadiryaman@gmail.com>
2015-09-21 12:54:17 +0300

    basic fuctionality for caching...

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,32 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  
+  set <- function(y = matrix()){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  
+  setinv <- function(inverseM) inv <<- inverseM
+  getinv <- function() inv
+  list(set = set, get = get, setinv = setinv, getinv  = getinv)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { 
+  inv <- x$getinv()
+  
+  if(!is.null(inv)){
+    message("getting cached inv matrix")
+    return(inv)
+  }
+  
+  data <- x$get()
+  inv <- solve(data) %*% data
+  x$setinv(inv)
+  inv
 }
